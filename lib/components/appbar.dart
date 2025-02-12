5d697390cf59904b534f22d38939ddbe56bb963e
Dhanraj Awatade <dhanuawatade@gmail.com>
2025-02-12 23:51:35 +0530

    Improved AppBar

--- lib/components/appbar.dart
+++ lib/components/appbar.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 AppBar myAppBar(String title, BuildContext context) => AppBar(
       // backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       title: Text(title),
       leading: IconButton(
-          onPressed: () => (), icon: const Icon(Icons.arrow_back_ios_rounded)),
+          onPressed: () => Navigator.pop(context), icon: const Icon(Iconsax.arrow_circle_left)),
     );
